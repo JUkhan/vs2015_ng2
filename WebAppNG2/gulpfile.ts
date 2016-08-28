77eb5d407cb12eb88e7e29c3c277b700de6bac1d
jukhan <jasim.uddin.khan@gmail.com>
2016-08-28 20:17:34 +0600

    intor

---
+++ WebAppNG2/gulpfile.ts
@@ -0,0 +1,61 @@
+
+var gulp = require('gulp');
+var rimraf = require('rimraf');
+var livereload = require('gulp-livereload');
+var paths = {
+    npmSrc: "node_modules",
+    libTargetAngular2: "scripts/angular2/",
+    libTargetRxjs: "scripts/rxjs/",
+    libTargetLodash: "scripts/lodash/"
+};
+
+var angularLibsToMove = [
+   paths.npmSrc + '/core-js/client/shim.min.js',
+   paths.npmSrc + '/zone.js/dist/zone.js',
+   paths.npmSrc + '/systemjs/dist/system.src.js',
+   paths.npmSrc + '/reflect-metadata/Reflect.js',
+   paths.npmSrc + '/@angular/**/*.js'
+];
+
+gulp.task('copy.libs', function () {
+    gulp.src(angularLibsToMove).pipe(gulp.dest(paths.libTargetAngular2));
+    gulp.src("node_modules/rxjs/**/*.js").pipe(gulp.dest(paths.libTargetRxjs));
+    //gulp.src("node_modules/lodash/**/*.js").pipe(gulp.dest(paths.libTargetLodash));
+    gulp.src("node_modules/lodash/lodash.min.js").pipe(gulp.dest(paths.libTargetLodash));
+   
+    return true;
+});
+
+gulp.task('remove.libs', function () {
+    [paths.libTargetAngular2, paths.libTargetRxjs, paths.libTargetLodash]
+    .forEach(path=> {
+        rimraf(path, () => { });
+    });
+    return true;
+});
+
+gulp.task('publish', function () {
+    rimraf('publish/', () => {
+        gulp.src('bin/**/*').pipe(gulp.dest('publish/bin'));
+        gulp.src('Views/**/*').pipe(gulp.dest('publish/Views'));
+        gulp.src('scripts/**/*').pipe(gulp.dest('publish/scripts/'));
+        gulp.src('app/**/*.js').pipe(gulp.dest('publish/app/'));
+        gulp.src('app/**/*.html').pipe(gulp.dest('publish/app/'));
+        gulp.src('app/**/*.css').pipe(gulp.dest('publish/app/'));
+        gulp.src('Content/**/*').pipe(gulp.dest('publish/Content'));
+        gulp.src('systemjs.config.js').pipe(gulp.dest('publish/'));
+        gulp.src('Web.config').pipe(gulp.dest('publish/'));
+        gulp.src('favicon.ico').pipe(gulp.dest('publish/'));
+        console.log('publishing...');
+    });
+
+});
+
+gulp.task('reload', function () {     
+    livereload.reload("/home");
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch('./app/**/*.js', ['reload']);
+});
--- WebAppNG2/systemjs.config.js
+++ WebAppNG2/systemjs.config.js
@@ -1,74 +1,40 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
 (function (global) {
-
-    // map tells the System loader where to look for things
     var map = {
-        'app': 'app', // 'dist',
-
+        'app': 'app',
         '@angular': 'scripts/angular2',
-        //'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs': 'scripts/rxjs',
         'lodash': 'scripts/lodash'
-
     };
-
-    // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'main.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
-        //'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
         'lodash': { main: 'lodash.min.js', defaultExtension: 'js' }
     };
-
     var ngPackageNames = [
-      'common',
-      'compiler',
-      'core',
-      'forms',
-      'http',
-      'platform-browser',
-      'platform-browser-dynamic',
-      'router',
-      'router-deprecated',
-      'upgrade',
+        'common',
+        'compiler',
+        'core',
+        'forms',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'router-deprecated',
+        'upgrade',
     ];
-
-    // Individual files (~300 requests):
     function packIndex(pkgName) {
         packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
     }
-
-    // Bundled (~40 requests):
     function packUmd(pkgName) {
         packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
     }
-
-    // Most environments should use UMD; some (Karma) need the individual index files
     var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-
-    // Add package entries for angular packages
     ngPackageNames.forEach(setPackageConfig);
-
-    // No umd for router yet
     packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents/juForm'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents/juGrid'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents/juMenu'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents/juPager'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents/juPanel'] = { main: 'index.js', defaultExtension: 'js' };
-    //packages['app/juComponents/juWindow'] = { main: 'index.js', defaultExtension: 'js' };
-
     var config = {
         map: map,
         packages: packages
     };
-
     System.config(config);
-   
-
 })(this);
-
+//# sourceMappingURL=systemjs.config.js.map
