b81117e177ce96aa575bb83adec063e76366d0b1
jukhan <jasim.uddin.khan@gmail.com>
2016-08-30 05:31:21 +0600

    updated gulpfile

--- WebAppNG2/gulpfile.js
+++ WebAppNG2/gulpfile.js
@@ -8,13 +8,24 @@ var paths = {
     libTargetRxjs: "scripts/rxjs/",
     libTargetLodash: "scripts/lodash/"
 };
-
+var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'forms',
+    'http',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+    'router-deprecated',
+    'upgrade',
+];
 var angularLibsToMove = [
-   paths.npmSrc + '/core-js/client/shim.min.js',
-   paths.npmSrc + '/zone.js/dist/zone.js',
-   paths.npmSrc + '/systemjs/dist/system.src.js',
-   paths.npmSrc + '/reflect-metadata/Reflect.js',
-   paths.npmSrc + '/@angular/**/*.js'
+    paths.npmSrc + '/core-js/client/shim.min.js',
+    paths.npmSrc + '/zone.js/dist/zone.js',
+    paths.npmSrc + '/systemjs/dist/system.src.js',
+    paths.npmSrc + '/reflect-metadata/Reflect.js'
+    //paths.npmSrc + '/@angular/**/*.js'
 ];
 
 gulp.task('copy.libs', function () {
@@ -22,15 +33,18 @@ gulp.task('copy.libs', function () {
     gulp.src("node_modules/rxjs/**/*.js").pipe(gulp.dest(paths.libTargetRxjs));
     //gulp.src("node_modules/lodash/**/*.js").pipe(gulp.dest(paths.libTargetLodash));
     gulp.src("node_modules/lodash/lodash.min.js").pipe(gulp.dest(paths.libTargetLodash));
-   
+    ngPackageNames.forEach(_ => {
+        gulp.src(`${paths.npmSrc}/@angular/${_}/bundles/*.min.js`).pipe(gulp.dest(`${paths.libTargetAngular2}/${_}/bundles/`));
+    });
+    gulp.src(`${paths.npmSrc}/@angular/router/**/*.js`).pipe(gulp.dest(`${paths.libTargetAngular2}/router/`));
     return true;
 });
 
 gulp.task('remove.libs', function () {
     [paths.libTargetAngular2, paths.libTargetRxjs, paths.libTargetLodash]
-    .forEach(path=> {
-        rimraf(path, () => { });
-    });
+        .forEach(path => {
+            rimraf(path, () => { });
+        });
     return true;
 });
 
@@ -51,7 +65,7 @@ gulp.task('publish', function () {
 
 });
 
-gulp.task('reload', function () {     
+gulp.task('reload', function () {
     livereload.reload("/home");
 });
 
