9efac6434ccad2c02f502ebce790e95609eab278
jasim <jasim@gmail.com>
2016-10-13 14:28:23 +0600

    change config

--- WebAppNG2/app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const core_1 = require('@angular/core');
-const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const app_module_1 = require('./app.module');
-core_1.enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-
-//# sourceMappingURL=main.js.map
--- WebAppNG2/src/setting/grid/crudExample.ts
+++ WebAppNG2/src/setting/grid/crudExample.ts
@@ -42,12 +42,12 @@ export class CrudExample implements OnInit {
     } 
     async hideCol() { 
         //this.service.messageDialog('Column removing', 'Column removed successfully');
-        if(await this.service.confirmDialogPromise('Column removing...', 'Are you sure to remove the age column?'))
+        if(await this.service.confirmDialogPromise('Column removing', 'Are you sure to remove the age column?'))
         {
             this.scholarGridOptions.columnDefs[3].hide = true;
             this.scholarGridOptions.api.grid.render();
         }
-        console.log('waiting for user input...');
+        console.log('waiting for user inputs.'); 
     }
     private onLoad(grid: juGrid)
     {         
